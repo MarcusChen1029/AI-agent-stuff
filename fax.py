fe8f7f8185ae4623fc7f1ca233f98d1e013a137a
MarcusChen1029 <86709415+MarcusChen1029@users.noreply.github.com>
2025-10-28 20:02:51 +0800

    Update fax.py

--- fax.py
+++ fax.py
@@ -1,26 +1,39 @@
 import requests
 import json
-import os
+
+import schedule
+import time
+
 file = "fax.json"
+
+
 try:
     with open(file, 'r') as f:
         fact_archive = json.load(f)
-except FileNotFoundError:
+except (FileNotFoundError, json.JSONDecodeError):
     fact_archive = []
 
 def get_stuff():
     url = "https://uselessfacts.jsph.pl/api/v2/facts/random"
     response = requests.get(url)
-
     response.raise_for_status()
-
     fact = response.json()
     return fact['text']
 
-if __name__ == "__main__":
+def do_thing():
+    global fact_archive
+
     fact = get_stuff()
-    if fact not in fact_archive:
-        fact_archive.append(fact)
-        with open(file, 'w') as f:
-            json.dump(fact_archive, f, indent=4)
+    while fact in fact_archive:
+        fact = get_stuff()
     print(fact)
+    fact_archive.append(fact)
+    with open(file, 'w') as f:
+        json.dump(fact_archive, f, indent=4)
+    
+
+if __name__ == "__main__":
+    schedule.every(10).seconds.do(do_thing)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
